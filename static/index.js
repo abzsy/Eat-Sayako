483c877945da9f9edeba97c9c68906048650033e
abzsy <101086457+abzsy@users.noreply.github.com>
2022-03-07 22:47:00 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -441,11 +441,11 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
             SubmitResults();
         }
 
-        if (cps <= 5) return '试着好好练一下？';
-        if (cps <= 8) return 'TCL';
-        if (cps <= 10)  return 'TQL';
-        if (cps <= 15) return '您';
-        return '人？';
+        if (cps <= 5) return '建议重开';
+        if (cps <= 8) return '那就摸摸菌';
+        if (cps <= 10)  return '我不好说';
+        if (cps <= 15) return '哇哦';
+        return '开了？';
     }
 
     function toStr(obj) {
