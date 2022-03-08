6b9f00f9d05899e29b41fe6f56bfdf8ef2a016d3
abzsy <101086457+abzsy@users.noreply.github.com>
2022-03-08 21:34:49 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -441,10 +441,10 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
             SubmitResults();
         }
 
-        if (cps <= 5) return '那就摸摸菌';
-        if (cps <= 8) return '哇哦';
-        if (cps <= 10)  return '侬要组撒？！';
-        if (cps <= 15) return '黑紗说话！';
+        if (cps <= 3) return '那就摸摸菌';
+        if (cps <= 5) return '哇哦';
+        if (cps <= 7)  return '侬要组撒？！';
+        if (cps <= 10) return '黑紗说话！';
         return '开了是吧？';
     }
 
